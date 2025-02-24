fb37c47f0eb137867129519b2c1609fdd7c8c4a6
LeviShinmon <adnionh@buffalo.edu>
2025-02-24 10:02:40 -0500

    laying the groundwork

---
+++ code.c
@@ -0,0 +1,40 @@
+#include <stdlib.h>
+// #include <stdio.h>
+// #include <string.h>
+
+
+
+void f() {}
+
+void r() {}
+
+void h() {}
+
+void max() {}
+
+void min() {}
+
+void mean() {}
+
+void records() {}
+
+void call_command(char command) {}
+
+void csv_file_reader(char csv_file) {}
+
+int main(int argc, char *argv[]) {
+    char *csv_file = argv[argc-1];
+    // create helper function to read csv file
+    for (int i = 0; i < argc-1; i++) { // might need to modify the way we're reading arguments
+        // i goes to argc-1 because all we want to get are the commands passed in
+
+        // call helper function that compares the entry to a some expected characters, and returns...
+        // get a return value
+
+
+
+
+        // return EXIT_FAILURE
+    }
+    return EXIT_SUCCESS;
+}

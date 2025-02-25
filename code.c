f60e22c6285daf7eba48a69029abb25193c0a75e
LeviShinmon <adnionh@buffalo.edu>
2025-02-25 11:50:26 -0500

    added some new logic to main and call command. still laying the groundwork

--- code.c
+++ code.c
@@ -1,4 +1,5 @@
 #include <stdlib.h>
+#include <stdbool.h>
 // #include <stdio.h>
 // #include <string.h>
 
@@ -18,23 +19,26 @@ void mean() {}
 
 void records() {}
 
-void call_command(char command) {}
+bool call_command(char command, char *csv_file) {
+    if (command == 'placeholder') {
+        bool command_return;
+        return command_return;
+    }
+}
 
-void csv_file_reader(char csv_file) {}
+// void csv_file_reader(char csv_file) {}
 
 int main(int argc, char *argv[]) {
     char *csv_file = argv[argc-1];
     // create helper function to read csv file
     for (int i = 0; i < argc-1; i++) { // might need to modify the way we're reading arguments
         // i goes to argc-1 because all we want to get are the commands passed in
+        //call_command(*argv[i], csv_file); the function call
+        // call helper function that compares the entry to a some expected characters, and returns a bool...
 
-        // call helper function that compares the entry to a some expected characters, and returns...
-        // get a return value
-
-
-
-
-        // return EXIT_FAILURE
+        if (!(call_command(*argv[i], csv_file))) {
+            return EXIT_FAILURE;
+        }
     }
     return EXIT_SUCCESS;
 }
